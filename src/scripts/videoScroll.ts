ea246f38a16e3cd72262af60f3c51465990a69fb
facubotta <nutuestampados@gmail.com>
2024-10-14 21:12:04 +0200

    working in pdf files

--- src/scripts/videoScroll.ts
+++ src/scripts/videoScroll.ts
@@ -1,21 +1,27 @@
+const skipAnimation = localStorage.getItem("skipAnimation");
+const video = document.getElementById("miVideo") as HTMLVideoElement;
+const videoContainer = document.getElementById(
+  "video-container"
+) as HTMLElement;
+
 export function setupVideoScroll() {
-  const video = document.getElementById("miVideo") as HTMLVideoElement;
-  const videoContainer = document.getElementById(
-    "video-container"
-  ) as HTMLElement;
-
   if (video) {
-    // Prevenir el scroll al principio
-    window.onscroll = function () {
-      window.scrollTo(0, 0);
-    };
-
-    video.currentTime = 0;
+    if (skipAnimation === "true") {
+      window.onscroll = null;
+      // TODO: aca deberia reajustar cuando cambie el video...
+      video.currentTime = 11.5;
+    } else {
+      // Prevenir el scroll al principio
+      window.onscroll = function () {
+        window.scrollTo(0, 0);
+      };
+      video.currentTime = 0;
+    }
     video.play();
 
-    // Reproducir automáticamente los primeros 11 segundos
+    // Reproducir automáticamente los primeros 11 segundos, cuando dejo de tipear
     const interval = setInterval(() => {
-      if (video.currentTime >= 11) {
+      if (video.currentTime >= 11.5) {
         video.pause();
         clearInterval(interval);
         // Después del segundo 11, permitir el control del video por scroll
@@ -23,19 +29,15 @@ export function setupVideoScroll() {
       }
     }, 100);
 
-    // Escuchar el evento scroll
     window.addEventListener("scroll", () => {
-      if (video.currentTime >= 11 && video.currentTime <= 22) {
-        // Durante los segundos 11 y 15, solo controlamos el video
+      if (video.currentTime >= 11.5 && video.currentTime <= 22) {
         syncVideoWithScroll();
       } else if (video.currentTime > 22) {
-        // A partir del segundo 15, también permitimos el scroll en la página
-        window.onscroll = null; // Permitimos el desplazamiento normal de la página
-        syncVideoWithScroll(); // Seguimos controlando el video con el scroll
+        window.onscroll = null;
+        syncVideoWithScroll();
       }
     });
 
-    // Función para sincronizar el video con el scroll
     const syncVideoWithScroll = () => {
       const scrollPosition = window.scrollY;
       const maxScroll = videoContainer.scrollHeight;
@@ -44,9 +46,8 @@ export function setupVideoScroll() {
       scrollFraction = Math.max(0, scrollFraction);
       scrollFraction = Math.min(1, scrollFraction);
 
-      // Controlar el video solo entre los segundos 11 y 15
-      const remainingTime = 11; // El intervalo es de 11 a 15 segundos
-      video.currentTime = 11 + remainingTime * scrollFraction;
+      const remainingTime = 11.5; // El intervalo es de 11 a 15 segundos
+      video.currentTime = 11.5 + remainingTime * scrollFraction;
 
       // Ajustar la opacidad del contenedor según el scroll
       videoContainer.style.opacity = (1 - scrollFraction).toString();
