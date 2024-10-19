d8981b08aac5f3d8d4b4a6ff9bca2edc33ebf2b6
facubotta <nutuestampados@gmail.com>
2024-10-19 15:12:23 +0200

    projects cards

---
+++ src/data/projects.ts
@@ -0,0 +1,43 @@
+import type { ProjectType } from "../types";
+
+export const projects: ProjectType[] = [
+  {
+    title: "Tasks Manager API",
+    description:
+      "A full API made in Node.js and Express.js that provides all endpoints needed to a task manager system.\
+      It's type safe and includes JWT authentication. This project brinds me to a deeper understanding of APIs and how to build them from scratch.",
+    image:
+      "https://s3.us-east-2.amazonaws.com/mgpanel/71-20221024-635708381cc8e.jpg",
+    link: "https://github.com/FacuBotta/Task-Management-API",
+    // TODO: add express icon and jwt icon
+    stack: ["NodeIcon", "TypesCriptIcon", "MysqlIcon"],
+  },
+  {
+    title: "Background Topics generator",
+    description:
+      "A little python project that generates backgrounds for windows about any of the topics of your choice. It use Ollama installed locally with llama LLM to generate the new\
+    content. I use it in my computer so I can have a different background for each day with different tips and tricks about the topics I'm learning.",
+    image:
+      "https://github.com/FacuBotta/background_generator/blob/main/assets/newBackgroung.jpg?raw=true",
+    link: "https://github.com/FacuBotta/background_generator/tree/main",
+    // TODO: add ollama icon
+    stack: ["PythonIcon"],
+  },
+  {
+    title: "Full Website with Admin page",
+    description:
+      "A full website for a theater company. It's a full CRUD app with admin panel and google authentication. It was my final project for my first year of web development\
+    studies and now is the base for the real website of the theater company but I'm migrating the project to react and node.js.",
+    image:
+      "https://github.com/FacuBotta/filrouge2023/raw/master/readmeFiles/image3.png",
+    link: "https://github.com/FacuBotta/filrouge2023",
+    stack: [
+      "PhpIcon",
+      "MysqlIcon",
+      "ApacheIcon",
+      "JavaScriptIcon",
+      "HtmlIcon",
+      "CssIcon",
+    ],
+  },
+];
