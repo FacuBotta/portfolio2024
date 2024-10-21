42e98700547b1e6f6ea98b9ccae30f6c7850a4d5
facubotta <nutuestampados@gmail.com>
2024-10-21 12:43:42 +0200

    translations ok

---
+++ src/data/myDescription.ts
@@ -0,0 +1,49 @@
+export const myDescription = {
+  en: "2 years as a fullstack web developer student, passionate by JavaScript and its ecosystem. Living in Montpellier, France.",
+  es: "2 años como estudiante de desarrollador web fullstack, apasionado por JavaScript y su ecosistema. Viviendo en Montpellier, Francia.",
+  fr: "2 ans en tant que étudiant en développement web fullstack, passionné par JavaScript et son écosystème. Vivant à Montpellier, France.",
+};
+
+export const aboutMe = {
+  en: [
+    "With over 6 years of freelance experience in Argentina, I've developed \
+    a strong sense of responsibility and quality work. Leading my teams \
+    has deepened my appreciation for teamwork and communication.",
+    "After moving to France in 2019, I worked for 3 years as a manager in a\
+    restaurant, enhancing my skills in teamwork and task management as\
+    lead of the team.",
+    "Since 2022, I've been pursuing my goal of becoming a web developer and am now in my final year of studies as Fullstack developer. I'm excited to apply my knowledge in real projects and work with a dev team.",
+  ],
+  fr: [
+    "Avec plus de 6 ans d'expérience en tant que freelance en Argentine,\
+    j'ai développé un fort sens du devoir et du travail de qualité.\
+    Diriger mes équipes a renforcé mon appréciation du travail d'équipe et\
+    de la communication.",
+
+    "Après avoir déménagé en France en 2019, j'ai travaillé pendant 3 ans\
+    en tant que manager dans un restaurant, perfectionnant mes compétences\
+    en gestion d'équipe et de tâches en tant que chef d'équipe.",
+
+    "Depuis 2022, je poursuis mon objectif de devenir développeur web et je\
+    suis actuellement dans ma dernière année d'études en tant que\
+    développeur Fullstack. Je suis enthousiaste à l'idée d'appliquer mes\
+    connaissances à des projets réels et de travailler avec une équipe de\
+    développeurs.",
+  ],
+  es: [
+    "Con más de 6 años de experiencia como freelance en Argentina, he\
+    desarrollado un fuerte sentido de responsabilidad y trabajo de\
+    calidad. Liderar mis equipos ha profundizado mi aprecio por el trabajo\
+    en equipo y la comunicación.",
+
+    "Después de mudarme a Francia en 2019, trabajé durante 3 años como\
+    gerente en un restaurante, mejorando mis habilidades en el trabajo en\
+    equipo y la gestión de tareas como líder del equipo.",
+
+    "Desde 2022, he estado persiguiendo mi objetivo de convertirme en\
+    desarrollador web y ahora estoy en mi último año de estudios como\
+    desarrollador Fullstack. Estoy emocionado por aplicar mis\
+    conocimientos en proyectos reales y trabajar con un equipo de\
+    desarrollo.",
+  ],
+};
---
+++ src/i18n/ui.ts
@@ -0,0 +1,67 @@
+export const languages = {
+  en: "English",
+  es: "Español",
+  fr: "Français",
+};
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    "nav.home": "Home",
+    "nav.projects": "Projects",
+    "nav.about": "About",
+    "nav.contact": "Contact",
+    "presentation.icon.link": ["Visit my LinkedIn", "Ctrl+Click to copy link"],
+    "presentation.icon.github": ["Check my GitHub!", "Ctrl+Click to copy link"],
+    "presentation.icon.gmail": [
+      "Email me via Gmail",
+      "Crl+Click to copy adresse",
+    ],
+    "presentation.icon.resume": "Download my CV",
+    "projects.repoBtn": "See repo",
+    "projects.stack": "Technologies used",
+  },
+  es: {
+    "nav.home": "Inicio",
+    "nav.projects": "Proyectos",
+    "nav.about": "Sobre mi",
+    "nav.contact": "Contacto",
+    "presentation.icon.link": [
+      "Visita mi LinkedIn",
+      "Ctrl+Click para copiar link",
+    ],
+    "presentation.icon.github": [
+      "¡Mira mi GitHub!",
+      "Ctrl+Click para copiar link",
+    ],
+    "presentation.icon.gmail": [
+      "Envíame un correo",
+      "Ctrl+Click para copiar dirección",
+    ],
+    "presentation.icon.resume": "Descarga mi CV",
+    "projects.repoBtn": "Ver repo",
+    "projects.stack": "Tecnologías usadas",
+  },
+  fr: {
+    "nav.home": "Accueil",
+    "nav.projects": "Projets",
+    "nav.about": "À propos",
+    "nav.contact": "Contact",
+    "presentation.icon.link": [
+      "Visite mon LinkedIn",
+      "Crl+Click pour copier le lien",
+    ],
+    "presentation.icon.github": [
+      "Vérifiez mon GitHub!",
+      "Crl+Click pour copier le lien",
+    ],
+    "presentation.icon.gmail": [
+      "Envoyez-moi un e-mail",
+      "Crl+Click pour copier l'adresse",
+    ],
+    "presentation.icon.resume": "Téléchargez mon CV",
+    "projects.repoBtn": "Voir repo",
+    "projects.stack": "Technologies utilisées",
+  },
+} as const;
+export const showDefaultLang = false;
