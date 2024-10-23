3d8a8a84ee81365d0f1718c5b9b56c43a0603220
facubotta <nutuestampados@gmail.com>
2024-10-23 12:32:56 +0200

    deleting readmeFiles to improve performance

--- src/content/projects/config.ts
+++
@@ -1,9 +0,0 @@
-import { defineCollection } from "astro:content";
-
-// Definir una colección de proyectos
-const projectsCollection = defineCollection({});
-
-// Exportar las colecciones
-export const collections = {
-  projects: projectsCollection,
-};
