316b3518c213399ec266671203cbc7051a097cad
facubotta <nutuestampados@gmail.com>
2024-10-23 20:49:21 +0200

    working in the translations texts. made by humans is cuter

--- src/data/myDescription.ts
+++ src/data/myDescription.ts
@@ -6,44 +6,30 @@ export const myDescription = {
 
 export const aboutMe = {
   en: [
-    "With over 6 years of freelance experience in Argentina, I've developed \
-    a strong sense of responsibility and quality work. Leading my teams \
-    has deepened my appreciation for teamwork and communication.",
-    "After moving to France in 2019, I worked for 3 years as a manager in a\
-    restaurant, enhancing my skills in teamwork and task management as\
-    lead of the team.",
-    "Since 2022, I've been pursuing my goal of becoming a web developer and am now in my final year of studies as Fullstack developer. I'm excited to apply my knowledge in real projects and work with a dev team.",
+    "With over 6 years of experience as a freelance in Argentina, I have developed a strong sense of responsibility and dedication to high-quality work. As a team leader, I have always emphasized the importance of cooperation and communication.",
+    "After moving to France in 2019, I worked for 3 years as a restaurant manager, applying my skills in teamwork and communication with both customers and suppliers.",
+    "Since 2022, I have been pursuing my goal of becoming a web developer and am about to obtain my second diploma. I am excited to apply my knowledge to real-world projects, and despite my lack of experience, I believe I have much to contribute to a dev team.",
   ],
   fr: [
-    "Avec plus de 6 ans d'expérience en tant que freelance en Argentine,\
-    j'ai développé un fort sens du devoir et du travail de qualité.\
-    Diriger mes équipes a renforcé mon appréciation du travail d'équipe et\
-    de la communication.",
+    "Avec plus de 6 ans d'expérience en tant que freelance en Argentine, j'ai développé un fort sens des responsabilités et du travail bien fait. En tant que chef d'équipe, j'ai toujours souligné l'importance de la coopération et de la communication.",
 
-    "Après avoir déménagé en France en 2019, j'ai travaillé pendant 3 ans\
-    en tant que manager dans un restaurant, perfectionnant mes compétences\
-    en gestion d'équipe et de tâches en tant que chef d'équipe.",
+    "Après avoir déménagé en France en 2019, j'ai travaillé pendant 3 ans en tant que gérant de restaurant, où j'ai mis en pratique mes compétences en travail d'équipe et en communication avec les clients et les fournisseurs.",
 
-    "Depuis 2022, je poursuis mon objectif de devenir développeur web et je\
-    suis actuellement dans ma dernière année d'études en tant que\
-    développeur Fullstack. Je suis enthousiaste à l'idée d'appliquer mes\
-    connaissances à des projets réels et de travailler avec une équipe de\
-    développeurs.",
+    "Depuis 2022, je poursuis mon objectif de devenir développeur web et je suis sur le point d'obtenir mon deuxième diplôme. Je suis enthousiaste à l'idée d'appliquer mes connaissances à des projets concrets et, malgré le manque d'expérience, je crois que j'ai beaucoup à apporter à une équipe.",
   ],
   es: [
     "Con más de 6 años de experiencia como freelance en Argentina, he\
-    desarrollado un fuerte sentido de responsabilidad y trabajo de\
-    calidad. Liderar mis equipos ha profundizado mi aprecio por el trabajo\
-    en equipo y la comunicación.",
+    desarrollado un fuerte sentido de la responsabilidad y del trabajo bien hecho.\
+    Como jefe de equipo siempre he remarcado la importancia de la cooperación\
+    y la comunicación.",
 
-    "Después de mudarme a Francia en 2019, trabajé durante 3 años como\
-    gerente en un restaurante, mejorando mis habilidades en el trabajo en\
-    equipo y la gestión de tareas como líder del equipo.",
+    "Después de mudarme a Francia en 2019, trabajé 3 años como\
+    gerente de un restaurante, aplicando mis habilidades para el trabajo en\
+    equipo y la comunicación con clientes y proveedores.",
 
-    "Desde 2022, he estado persiguiendo mi objetivo de convertirme en\
-    desarrollador web y ahora estoy en mi último año de estudios como\
-    desarrollador Fullstack. Estoy emocionado por aplicar mis\
-    conocimientos en proyectos reales y trabajar con un equipo de\
-    desarrollo.",
+    "Desde 2022, persigo mi objetivo de convertirme en\
+    desarrollador web y estoy por obtener mi segundo diploma. Estoy emocionado por aplicar mis\
+    conocimientos en proyectos reales y, pese a la falta de experiencia, creo\
+    que tengo mucho que aportar a un grupo de trabajo.",
   ],
 };
